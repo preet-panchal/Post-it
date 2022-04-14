4ea244d228ff479954038605c93e89aec6374303
Eihab Syed <eihabsyed@gmail.com>
2022-04-14 00:54:23 -0400

    Added error messages

---
+++ client/src/components/ErrorPopup.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="error-wrapper">
+    <div class="error">
+      {{ msg }}
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+    name: "ErrorPopup",
+    props: {
+        msg: String
+    }
+}
+
+</script>
+
+
+<style>
+.error-wrapper {
+  position: fixed;
+  width: 100%;
+  top: 15%;
+  z-index: 5;
+}
+.error {
+  padding: 25px;
+  color: white;
+  background: #ff0062;
+  border-radius: 10px;
+  box-shadow: 1px 3px 5px rgba(0,0,0,0.2);
+  max-width: 500px;
+  margin: 0 auto;
+}
+</style>
---
+++ client/src/components/SuccessPopup.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="success-wrapper">
+    <div class="success">
+      {{ msg }}
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+    name: "SuccessPopup",
+    props: {
+        msg: String
+    }
+}
+
+</script>
+
+
+<style>
+.success-wrapper {
+  position: fixed;
+  width: 100%;
+  top: 15%;
+  z-index: 5;
+}
+.success {
+  padding: 25px;
+  color: white;
+  background: #198754;
+  border-radius: 10px;
+  box-shadow: 1px 3px 5px rgba(0,0,0,0.2);
+  max-width: 500px;
+  margin: 0 auto;
+}
+</style>
