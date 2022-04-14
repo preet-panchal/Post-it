2673aa5124734a351d9cfc4972df5b803ecc8bb0
Eihab Syed <eihabsyed@gmail.com>
2022-04-14 00:50:24 -0400

    Updated ProfileView

--- client/src/views/ProfileView.vue
+++ client/src/views/ProfileView.vue
@@ -1,49 +1,93 @@
 <template>
     <div>
+        <transition name="error">
+            <ErrorPopup v-if="failure" msg="Error: Title must be unique and body cannot be empty!"/>
+        </transition>
+        <transition name="success">
+            <SuccessPopup v-if="success" msg="Success: Your post went through!"/>
+        </transition>
         <h1>Log in to see your posts and to create new posts!</h1>
         <div class="dashboard">
-            <h3 v-on:click="showDashboard" class="option">Dashboard</h3> |
-            <h3 v-on:click="showCreate" class="option">Create</h3> |
-            <h3 v-on:click="showAnalytics" class="option">Analytics</h3>
+            <h3 v-on:click="showDashboard" :class="{ active: isDashboardHidden }" class="option">Dashboard</h3> |
+            <h3 v-on:click="showCreate" :class="{ active: isCreateHidden }" class="option">Create</h3> |
+            <h3 v-on:click="showAnalytics" :class="{ active: isAnalyticsHidden }" class="option">Analytics</h3>
         </div>
   
         <div v-if="isDashboardHidden" class="content">
-            <p>Dashboard</p>
+            <h1>Dashboard</h1>
+            <strong>Your posts show up here.</strong>
+            <div v-for="post in usersPosts" :key="post.postid" class="post-card">
+                <div class="title">
+                    <p>{{post.title}}</p>
+                </div>
+                <hr>
+                <div class="author">
+                    <p>{{post.author}}</p>
+                </div>
+                <hr>
+                <div class="body">
+                    <p>{{post.body}}</p>
+                </div>
+                <hr>
+                <div class="interact">
+                    <button><i class="fa-solid fa-chevron-up btn"></i></button>
+                    <p class="views">{{post.upvotes}}</p>
+                    <button><i class="fa-solid fa-chevron-down btn"></i></button>
+                    <p class="views">{{post.downvotes}}</p>
+                    <button><i class="fa-solid fa-x x"></i></button>
+                </div>
+            </div>
         </div>
-        <div v-if="isCreateHidden" class="content">
+        <form v-if="isCreateHidden" class="content">
+            <strong>You can create posts here.</strong>
             <div class="title">
                 <p>Post Title:</p>
-                <input v-model="title" placeholder="Title">
+                <input id="post-title" v-model="title" placeholder="Title">
             </div>
-            <div class="body">
+            <div class="textArea">
                 <p>Post Body:</p>
-                <textarea v-model="body" placeholder="Post Body"></textarea>
+                <textarea id="post-body" v-model="body" placeholder="Post Body"></textarea>
             </div>
-            <button class="button">Post-It!</button>
-        </div>
+            <button class="button" @click.prevent="createPost">Post-It!</button>
+        </form>
         <div v-if="isAnalyticsHidden" class="content">
-            <p>Anayltics</p>
+            <h1>Anayltics</h1>
+            <strong>Here are your analytics.</strong>
         </div>
     </div>
 </template>
 
 <script>
+import ErrorPopup from '../components/ErrorPopup.vue'
+import SuccessPopup from '../components/SuccessPopup.vue'
+import { api } from '../apis/api';
 
 export default {
     name: "ProfileView",
+    components: {
+        ErrorPopup,
+        SuccessPopup
+    },
     data() {
         return {
             isHidden: false,
             isDashboardHidden: true,
             isCreateHidden: false,
-            isAnalyticsHidden: false
+            isAnalyticsHidden: false,
+            usersPosts: [],
+            title: "",
+            body: "",
+            user: [],
+            success: false,
+            failure: false
         }
     },
     methods: {
-        showDashboard() {
+        async showDashboard() {
             this.isDashboardHidden = true;
             this.isCreateHidden = false;
             this.isAnalyticsHidden = false;
+            this.usersPosts = await api.getPostsByUser('6257397b5d0e39067499ad30');
         },
         showCreate() {
             this.isDashboardHidden = false;
@@ -54,7 +98,25 @@ export default {
             this.isDashboardHidden = false;
             this.isCreateHidden = false
             this.isAnalyticsHidden = true;
+        },
+        async createPost() {
+            var payload = {title: this.title, body: this.body, upvotes: 0, downvotes: 0, author: this.user[0].firstName + " " + this.user[0].lastName, userid: '6257397b5d0e39067499ad30'};
+            try {
+                await api.createPost(payload);
+                this.title = "";
+                this.body = "";
+                this.success = true;
+                setTimeout(() => this.success = false, 2000);
+            } catch (e) {
+                console.log(e);
+                this.failure = true;
+                setTimeout(() => this.failure = false, 2000);
+            }
         }
+    },
+    async mounted() {
+        this.usersPosts = await api.getPostsByUser('6257397b5d0e39067499ad30');
+        this.user = await api.getUserById('6257397b5d0e39067499ad30');
     }
 };
 </script>
@@ -77,7 +139,7 @@ export default {
     margin-top: 50px;
 }
 
-.body {
+.textArea {
     padding-bottom: 20px;
     font-size: 25px;
 }
@@ -91,4 +153,98 @@ input {
     width: 400px;
     height: 30px;
 }
+
+.post-card {
+  border: solid 1px black;
+  width: 900px;
+  margin: auto;
+  background: #ffffff;
+  padding: 10px;
+  border-radius: 10px;
+  margin: 20px auto;
+  max-width: 800px;
+  color: #444;
+}
+
+.interact {
+  display: inline-flex;
+  margin-right: 100%;
+}
+
+.btn, .views {
+  padding: 0px 5px;
+}
+
+hr {
+  height: 3px;
+  background-color: #ccc;
+}
+
+.title {
+  font-size: 25px;
+  padding-top: 15px;
+}
+
+.author {
+  font-size: 20px;
+}
+
+.active {
+    color: #fbd758;
+}
+
+.x {
+  color: red;
+}
+
+/* enter transitions */
+  .error-enter-from {
+    opacity: 0;
+    transform: translateY(-400px);
+  }
+  /* .error-enter-to {
+    opacity: 1;
+    transform: translateY(0);
+  } */
+  .error-enter-active {
+    transition: all 1s ease;
+  }
+  /* leave transitions */
+  /* .error-leave-from {
+    opacity: 1;
+    transform: translateY(0);
+  } */
+  .error-leave-to {
+    opacity: 0;
+    transform: translateY(-400px);
+  }
+  .error-leave-active {
+    transition: all 0.5s ease;
+  }
+
+
+  /* enter transitions */
+  .success-enter-from {
+    opacity: 0;
+    transform: translateY(-400px);
+  }
+  /* .success-enter-to {
+    opacity: 1;
+    transform: translateY(0);
+  } */
+  .success-enter-active {
+    transition: all 1s ease;
+  }
+  /* leave transitions */
+  /* .success-leave-from {
+    opacity: 1;
+    transform: translateY(0);
+  } */
+  .success-leave-to {
+    opacity: 0;
+    transform: translateY(-400px);
+  }
+  .success-leave-active {
+    transition: all 0.5s ease;
+  }
 </style>
