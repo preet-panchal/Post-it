d9c8d012ce32c0e3a3ae2ce1a45bb4c1871a1178
Preet Panchal <panchalpreet094@gmail.com>
2022-04-15 15:37:53 -0400

    added-arrow-animation

--- client/src/components/HomePage.vue
+++ client/src/components/HomePage.vue
@@ -11,7 +11,13 @@
           <div>
             <p>Stay connected to the world.</p>
             <p>Scroll down to see trending posts!</p>
+            <div class="container">
+              <div class="chevron"></div>
+              <div class="chevron"></div>
+              <div class="chevron"></div>
+            </div>
           </div>
+          
         </div>
     </div>
     <div class="header">
@@ -42,7 +48,7 @@
 
 <script>
 //import axios from 'axios';
-import { api } from '../apis/api';
+// import { api } from '../apis/api';
 
 export default {
   name: 'HomePage',
@@ -56,9 +62,9 @@ export default {
   },
   computed: {
   },
-  async mounted() {
-    this.posts = await api.getPosts();
-  }
+  // async mounted() {
+  //   this.posts = await api.getPosts();
+  // }
 }
 </script>
 
@@ -143,6 +149,70 @@ hr {
     100% {
       background-size: 300% 300%;
     }
+  }
+}
+
+$base: 0.6rem;
+
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.chevron {
+  position: absolute;
+  width: $base * 3.5;
+  height: $base * 0.8;
+  opacity: 0;
+  transform: scale(0.3);
+  animation: move-chevron 3s ease-out infinite;
+}
+
+.chevron:first-child {
+  animation: move-chevron 3s ease-out 1s infinite;
+}
+
+.chevron:nth-child(2) {
+  animation: move-chevron 3s ease-out 2s infinite;
+}
+
+.chevron:before,
+.chevron:after {
+  content: '';
+  position: absolute;
+  top: 0;
+  height: 100%;
+  width: 50%;
+  background: #fff  ;
+}
+
+.chevron:before {
+  left: 0;
+  transform: skewY(30deg);
+}
+
+.chevron:after {
+  right: 0;
+  width: 50%;
+  transform: skewY(-30deg);
+}
+
+@keyframes move-chevron {
+  25% {
+    opacity: 1;
+  }
+  33.3% {
+    opacity: 1;
+    transform: translateY($base * 3.8);
+  }
+  66.6% {
+    opacity: 1;
+    transform: translateY($base * 5.2);
+  }
+  100% {
+    opacity: 0;
+    transform: translateY($base * 8) scale(0.5);
   }
 }
 </style>
