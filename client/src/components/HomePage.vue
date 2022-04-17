92862216129fbcb133ad51eddcf29df9c3760ba0
Eihab Syed <eihabsyed@gmail.com>
2022-04-17 00:10:54 -0400

    Update HomePage.vue

--- client/src/components/HomePage.vue
+++ client/src/components/HomePage.vue
@@ -59,27 +59,27 @@ export default {
   data() {
     return {
       posts: [
-                {
-                    "_id": "6258a6f7303e638a4ec62df9",
-                    "title": "Test1",
-                    "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                    "upvotes": 0,
-                    "downvotes": 0,
-                    "author": "Preet Panchal",
-                    "userid": "6257397b5d0e39067499ad30",
-                    "v": 0
-                },
-                {
-                    "_id": "62599885fd0c40f912299d70",
-                    "title": "sdf",
-                    "body": "sdf",
-                    "upvotes": 0,
-                    "downvotes": 0,
-                    "author": "Preet Panchal",
-                    "userid": "6257397b5d0e39067499ad30",
-                    "v": 0
-                }
-            ]
+        {
+          "_id": "6258a6f7303e638a4ec62df9",
+          "title": "Test1",
+          "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+          "upvotes": 0,
+          "downvotes": 0,
+          "author": "Preet Panchal",
+          "userid": "6257397b5d0e39067499ad30",
+          "v": 0
+        },
+        {
+          "_id": "62599885fd0c40f912299d70",
+          "title": "sdf",
+          "body": "sdf",
+          "upvotes": 0,
+          "downvotes": 0,
+          "author": "Preet Panchal",
+          "userid": "6257397b5d0e39067499ad30",
+          "v": 0
+        }
+      ]
     }
   },
   computed: {
