76bd9e4888fa0c8c2c215cdc520e9bdd5648f7af
Preet Panchal <panchalpreet094@gmail.com>
2022-04-17 11:47:10 -0400

    Update AboutView.vue

--- client/src/views/AboutView.vue
+++ client/src/views/AboutView.vue
@@ -81,6 +81,24 @@ export default {
     }
   }
 };
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal-content");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
 </script>
 
 <style lang="scss" scoped>
