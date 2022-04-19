882ad0d9fba34e2f97be669033fba98ba5be2558
Eihab Syed <eihabsyed@gmail.com>
2022-04-18 23:53:27 -0400

    Update DashboardPage.vue

--- client/src/components/DashboardPage.vue
+++ client/src/components/DashboardPage.vue
@@ -2,6 +2,16 @@
     <div>
         <h1>Your Bio</h1>
         <strong>Your posts show up here.</strong>
+        <div v-for="post in user" :key="post._id" class="post-card">
+          <div class="wrapper">
+              <div class="blog_post">
+                <div class="container_copy">
+                  <p>{{post.firstName}}</p>
+                  <p>{{post.lastName}}</p>
+                </div>
+              </div>
+          </div>
+        </div>
     </div>
     
 </template>
@@ -10,7 +20,7 @@
 export default {
     name: 'DashboardPage',
     props: {
-        user: []
+        user: Array
     }
 }
 </script>
