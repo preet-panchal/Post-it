c74dbd79a38c0c8b3061f784b11155293b023d83
Eihab Syed <eihabsyed@gmail.com>
2022-04-18 23:52:41 -0400

    Update HomePage.vue

--- client/src/components/HomePage.vue
+++ client/src/components/HomePage.vue
@@ -48,8 +48,7 @@
 </template>
 
 <script>
-// import axios from 'axios';
-/* import { api } from '../apis/api'; */
+import { api } from '../apis/api';
 
 export default {
   name: 'HomePage',
@@ -59,7 +58,7 @@ export default {
   data() {
     return {
       posts: [
-        {
+        /* {
           "_id": "6258a6f7303e638a4ec62df9",
           "title": "Test1",
           "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
@@ -78,14 +77,14 @@ export default {
           "author": "Preet Panchal",
           "userid": "6257397b5d0e39067499ad30",
           "v": 0
-        }
+        } */
       ]
     }
   },
   computed: {
   },
   async mounted() {
-    /* this.posts = await api.getPosts(); */
+    this.posts = await api.getPosts();
   }
 }
 </script>
