677655470776f41db98dc4a4845bc70ad4da51cd
Preet Panchal <panchalpreet094@gmail.com>
2022-04-23 22:53:21 -0400

    Update AppNavigation.vue

--- client/src/components/AppNavigation.vue
+++ client/src/components/AppNavigation.vue
@@ -69,9 +69,9 @@ export default {
 .button {
     transition: 0.5s;
     background-size: 200% auto;
-    /* background-color: #fbd758;
-    color: #36476b; */
-    background-image: linear-gradient(to right, #fbd758 0%, #36476b 100%, #fbd758 0%);
+    background-color: #fbd758;
+    color: #36476b; 
+    // background-image: linear-gradient(to right, #fbd758 0%, #36476b 100%, #fbd758 0%);
     &:hover {
         /* background-color: #ffffff;
         color: #36476b; */
