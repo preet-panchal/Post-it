8003de1fa92d3c1ae0bb57805c49508b889f3d0b
Eihab Syed <eihabsyed@gmail.com>
2022-04-27 01:55:11 -0400

    Added .env file for database

--- server/database/database.js
+++ server/database/database.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose')
+require('dotenv').config();
 
 mongoose.Promise = global.Promise;
 const connectDB = async () => {
     try {
-      const conn = await mongoose.connect('mongodb+srv://EihabSyed:Post-It@post-it.7es3b.mongodb.net/Post-It?retryWrites=true&w=majority')
+      const conn = await mongoose.connect(process.env.URI_KEY)
       console.log(`MongoDB Connected: ${conn.connection.host}`)
     } catch (error) {
       console.log(error)
